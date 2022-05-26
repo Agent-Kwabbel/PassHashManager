dfc8005e730cc842e91588a2c677c270d8a32288
Agent Kwabbel <50133696+Agent-Kwabbel@users.noreply.github.com>
2022-05-26 18:59:49 +0200

    Update app.py
    Making the code a little bit clearer and adding/changing some notes

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-# Imports
 import hashlib
 import os
 
 users = {}  # Simple User Storage
 print("PassHashManager successfully up and running")
 
-# Add a user
-print("Creating test account")
-username = input("> Username: ")
-password = input("> Password: ")
+# Creating an account
+print("Creating account")
+username = input("> Username: ")  # New account username input
+password = input("> Password: ")  # New account password input
 
-# Creating salt and key
+# Creating salt and a key
 salt = os.urandom(32)
 key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
 users[username] = {  # Storing salt and key
@@ -18,20 +17,22 @@ users[username] = {  # Storing salt and key
     'key': key
 }
 
-# Verification
+# Login
 print("Test login")
-username = input("> Check Username: ")
-password = input("> Check Password: ")
+username = input("> Check Username: ")  # Login username input
+password = input("> Check Password: ")  # Login password input
 
-if username in users:  # Check if username is found
-    salt = users[username]['salt']  # Loading salt
-    key = users[username]['key']  # Loading Key
+# Check if username is found
+if username in users:  # Username found
+    salt = users[username]['salt']  # Loading salt of username
+    key = users[username]['key']  # Loading key of username
     login_key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)  # Creating login key
 
-    # Cross-referencing login-key with stored username key
+    # Cross-referencing login-key with stored key
     if key == login_key:  # Login-key same as stored username key (Correct)
         print("Password was correct.")
     else:  # Login-key was not the same as stored username key (Incorrect)
         print("Password was incorrect.")
+
 else:  # Username not found
     print("Username not found.")
